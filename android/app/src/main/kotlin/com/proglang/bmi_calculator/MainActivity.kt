d292257db74e61c56bde4ff13f441593422a86e5
nikhil-2017 <nickgaikwad11@gmail.com>
2020-10-04 12:25:28 +0530

    create bmi_calculator

---
+++ android/app/src/main/kotlin/com/proglang/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.proglang.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 60.0;
+const kBottomContainerColor = Color(0xFFEB1555);
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kBottomButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kNormalBMITextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Color(0xFF4C4F5E),
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMIResultTextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
